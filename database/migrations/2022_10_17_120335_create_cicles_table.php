3f823dad4e3de8b212e7b8732ae8a4560f0e7cc8
Andres Molina <andresitomolinaclan@gmail.com>
2022-10-18 09:38:48 +0200

    Insertado lo de los compañeros

---
+++ database/migrations/2022_10_17_120335_create_cicles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCiclesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cicles', function (Blueprint $table) {
+            $table->bunsignedBigIncrements('id');
+            $table->string('name',255);
+            $table->string('img',255);
+            $table->boolean('deleted');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cicles');
+    }
+}
