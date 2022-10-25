7f1929418b80d26a2edef243482d9a44b30fbe38
Andres Molina <andresitomolinaclan@gmail.com>
2022-10-25 09:01:41 +0200

    Ya se puede insertar los datos automaticamente

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        factory(\App\Cicles::class, 20)->create();
+        factory(\App\Articles::class, 50)->create();
+        factory(\App\User::class, 10)->create();
+        factory(\App\Offers::class, 50)->create();
+        factory(\App\Requirements::class, 20)->create();
+        factory(\App\Applied::class, 50)->create();
     }
 }
