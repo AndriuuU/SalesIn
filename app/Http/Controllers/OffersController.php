ba08ff4abffde417e04392f5e069a294347e4e7c
Andres Molina <andresitomolinaclan@gmail.com>
2023-01-10 10:01:06 +0100

    Arreglado

--- app/Http/Controllers/OffersController.php
+++ app/Http/Controllers/OffersController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use App\Cicles;
 use App\Offers;
+use App\Applieds;
 use WithFileUploads;
 
 class OffersController extends Controller
@@ -15,8 +16,32 @@ class OffersController extends Controller
     public function index(Request $request){
 
         $offers=Offers::paginate(10);
-		return view('offers.index', compact('offers'));
-		
-		
+        $cicles=cicles::all();
+		return view('offers.index', compact('offers','cicles'));
+
+	}
+    
+
+    public function aplicar(Request $request,Offers $id){
+        $user = $request->user();
+        $applied = Applieds::all();
+
+        if($applied->offer_id == $id && $applied->user_id == $user){
+
+			
+				return redirect()->route('users.index')->with('message', 'FELICIDADES! Usuario Validado correctamente');
+				
+			}else{
+				return redirect()->route('users.index')->with('messageError', 'ERROR!!! Usuario No ha verificado el correo');
+			}
+		}else {
+			$user->actived = 0;
+			$user->update();
+			return redirect()->route('users.index')->with('message', 'FELICIDADES! Usuario desvalidado correctamente');
+		}
+
+        
+		return view('offers.index', compact('offers','cicles'));
+
 	}
 }
--- routes/web.php
+++ routes/web.php
@@ -48,6 +48,9 @@ Route::post('/noticias/create/add', 'ArticlesController@store')->name('articles.
 
 //ofertas
 Route::get('/ofertas', 'OffersController@index');
+Route::post('/ofertas/aplicar/{offer}', 'OffersController@aplicar');
+Route::get('pdf', 'InformesController@prueba')->name('pdf');
+
 
 
 Auth::routes();
